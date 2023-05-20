b420f9836b780a6727d079f724a4b074ffa1635f
Hyunjae Kim <khj99present@gmail.com>
2023-05-21 00:01:28 +0900

    coding test: Prefix Sum

---
+++ Num11659.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 구간 합 구하기 4 (백준 온라인 저지 11659번)
+ * https://www.acmicpc.net/problem/11659
+ */
+
+public class Num11659 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+        int suNo = Integer.parseInt(stringTokenizer.nextToken());
+        int quizNo = Integer.parseInt(stringTokenizer.nextToken());
+
+        long[] S = new long[suNo + 1];
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        for (int i = 1; i <= suNo; i++) {
+            S[i] = S[i - 1] + Integer.parseInt(stringTokenizer.nextToken());
+        }
+        for (int q = 0; q < quizNo; q++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int i = Integer.parseInt(stringTokenizer.nextToken());
+            int j = Integer.parseInt(stringTokenizer.nextToken());
+            System.out.println(S[j] - S[i-1]);
+        }
+
+    }
+}
--- Num1546.java
+++ Num1546.java
@@ -10,17 +10,19 @@ public class Num1546 {
         Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
-        int[] arr = new int[N];
+        int A[] = new int[N];
         for (int i = 0; i < N; i++) {
-            arr[i] = sc.nextInt();
+            A[i] = sc.nextInt();
         }
-
+        
         long sum = 0;
         long max = 0;
         for (int i = 0; i < N; i++) {
-            if (arr[i] > max) max = arr[i];
-            sum = sum + arr[i];
+            if (A[i] > max) max = A[i];
+            sum = sum + A[i];
         }
-        System.out.println(sum * 100.0 / max / N);
+        
+        // 한 과목과 관련된 수식을 총합한 후 관련된 수식으로 변환해 로직이 간단해짐
+        System.out.println(sum * 100.0 / max /N);
     }
 }
